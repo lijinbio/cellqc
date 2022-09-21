6a4e3db1992533bbc32168827f78e051a886c851
Jin Li <lijin.abc@gmail.com>
2022-09-20 23:14:38 -0500

    tests: fix data.sh

--- tests/data.sh
+++ tests/data.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 # vim: set noexpandtab tabstop=2:
 
-set -xe
-mkdir -p /tmp/cellqc && cd /tmp/cellqc
+source trapdebug
+outdir=$(mrrdir.sh)/test_data
+mkdir -p "$outdir" && cd "$outdir" 
 wget -O scPred_reference.rds https://bcm.box.com/shared/static/jtmx0lgvsxhcht0ycsccnumx3v2y8ett.rds
 wget -O cellqc_test_data.zip https://bcm.box.com/shared/static/19d41zw8tdcf0p82rs90607paklf54ym.zip
 unzip cellqc_test_data.zip
+
+cat > samples.txt <<EOF
+sample	cellranger
+AMD1	"$outdir/cellqc_test_data/AMD1"
+AMD2	"$outdir/cellqc_test_data/AMD2"
+EOF
--- tests/main.sh
+++ tests/main.sh
@@ -2,22 +2,5 @@
 # vim: set noexpandtab tabstop=2:
 
 source trapdebug
-indir=$(mrrdir.sh ..)
 outdir=$(mrrdir.sh)
-
-function cmd {
-local f=$1
-local bname=$(basename "$f" .h5ad)
-
-if fileexists.sh "$f"
-then
-	slurmtaco.sh -t 2 -m 20G --
-fi
-}
-
-source env_parallel.bash
-env_parallel cmd ::: "$indir"/*.h5ad
-
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda init && conda activate "$condaenv"
-
+cellqc -c config.yaml -d "$outdir"
